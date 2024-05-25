54ff3f004f9e5db7a39ba26675cc85d0aaa98c15
Kush <kushsinghdsc@gmail.com>
2024-05-26 00:24:27 +0530

    added filtering, using selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil"
-import { todoListState } from "./store/atoms/todos"
+import { filteredTodoListState, todoListFilterState, todoListState, todoListStatsState } from "./store/atoms/todos"
 import { useState } from "react";
 
 function App() {
@@ -15,12 +15,12 @@ function App() {
 
 function TodoList() {
   //console.log("insiden todoList")
-  const todoList = useRecoilValue(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState);
 
   return (
     <>
-    {/* <TodoListStats /> */}
-    {/* <TodoListFilters /> */}
+    <TodoListStats />
+    <TodoListFilters />
     <TodoItemCreator />
 
     {todoList.map((todoItem) => (
@@ -107,6 +107,45 @@ function replaceItemAtIndex(arr, index, newValue) {
 
 function removeItemAtIndex(arr, index) {
   return [...arr.slice(0, index), ...arr.slice(index + 1)];
+}
+
+function TodoListFilters() {
+  const [filter, setFilter] = useRecoilState(todoListFilterState);
+
+  const updateFilter = ({target: {value}}) => {
+    setFilter(value);
+  };
+
+  return (
+    <>
+    Filter:
+    <select value={filter} onChange={updateFilter}>
+      <option value="Show All">All</option>
+      <option value="Show Completed">Completed</option>
+      <option value="Show Uncompleted">Uncompleted</option>
+    </select>
+    </>
+  )
+}
+
+function TodoListStats() {
+  const {
+    totalNum,
+    totalCompletedNum,
+    totalUncompletedNum,
+    percentCompleted,
+  } = useRecoilValue(todoListStatsState);
+
+  const formattedPercentCompleted = Math.round(percentCompleted);
+
+  return (
+    <ul>
+      <li>Total items: {totalNum}</li>
+      <li>Items completed: {totalCompletedNum}</li>
+      <li>Items not completed: {totalUncompletedNum}</li>
+      <li>Percent completed: {formattedPercentCompleted}</li>
+    </ul>
+  );
 }
 
 export default App
